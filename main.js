63c00f3b9ed87fba647cac905b68e9d6250afbc8
ssp013 <ssp013sanchez@gmail.com>
2021-11-02 22:08:09 -0300

    others with POO Js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 //import User from "./class";
 const User = require("./class.js");//commún
 const hitesh = new User("Seba","ssp013@gmail.com");
-hitesh.courseList.push("math");
-console.log(hitesh.getCourselist());
+hitesh.enrollCourse("Math");
+hitesh.enrollCourse("Biology");
+hitesh.enrollCourse("Spanish");
+let courses =hitesh.getCourselist();
+courses.forEach( (c) => console.log(c)); 
